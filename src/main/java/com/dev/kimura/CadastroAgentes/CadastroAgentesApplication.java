1a3fa345bdded1d3e60c7f8a9ce9b8e0978da0b7
Rafael <rafaelkimura57@gmail.com>
2025-06-26 23:27:00 -0300

    first commit

---
+++ src/main/java/com/dev/kimura/CadastroAgentes/CadastroAgentesApplication.java
@@ -0,0 +1,13 @@
+package com.dev.kimura.CadastroAgentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroAgentesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroAgentesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/kimura/CadastroAgentes/CadastroAgentesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.kimura.CadastroAgentes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroAgentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
