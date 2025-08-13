686454427fc2d193c49c4d53033de5539789e8b2
Kimura <rafaelkimura57@gmail.com>
2025-08-13 00:42:16 -0300

    feature: Adicionando V1 para que o flyway seja encarregado de criar as minhas tabelas

--- src/main/java/com/dev/kimura/CadastroAgentes/Agentes/AgenteController.java
+++ src/main/java/com/dev/kimura/CadastroAgentes/Agentes/AgenteController.java
@@ -1,8 +1,6 @@
 package com.dev.kimura.CadastroAgentes.Agentes;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,4 +10,38 @@ public class AgenteController {
     public String bemVindo(){
         return "Bem vindo! este projeto é obra de Rafael Kimura, e provavelmente ele ainda estava treinando Spring!";
     }
+
+    // Adicionar um Agente (Create)
+    @PostMapping("/criar")
+    public String criarAgente(){
+        return "algo";
+    }
+
+    // Exibir todos os Agentes (Read)
+    @GetMapping("/todos")
+    public String exibirTodosAgentes(){
+        return "Mostrar todos agentes";
+    }
+
+    // Exibir apenas um Agente (Read)
+    @GetMapping("/todosID")
+    public String exibirAgentePorID(){
+        return "Mostrar agentes por ID";
+    }
+
+    // Alterar os dados dos Agentes (Update)
+    @PutMapping ("/alterarID")
+    public String alteraAgentePorID(){
+        return "Alterar agentes por ID";
+    }
+
+    // Deletar um Agente (Delete)
+
+    @DeleteMapping ("/deletarID")
+    public String deletarAgentePorID(){
+        return "Agente Deletado por ID";
+    }
+
+
+
 }
--- src/main/resources/DB/Migrations/V2__Add_rank_tarefas.sql
+++ src/main/resources/DB/Migrations/V2__Add_rank_tarefas.sql
@@ -1,4 +1,4 @@
 --V2: Migration para adicionar a coluna de Rank na tabela de tarefas
---Uma vez executado este arquivo se torna imutável, então qualquer adição neste arquivo não surtirá efeito
+
 ALTER TABLE tarefas
 ADD COLUMN rank VARCHAR(255);
