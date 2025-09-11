ccb69b5c6092c7b2e1f0f936cea01daf26f239c2
Kimura <rafaelkimura57@gmail.com>
2025-09-11 00:21:30 -0300

    refactor: Adicionando responseEntity nos controllers para melhor resposta para o usuário

--- src/main/java/com/dev/kimura/CadastroAgentes/Agentes/AgenteController.java
+++ src/main/java/com/dev/kimura/CadastroAgentes/Agentes/AgenteController.java
@@ -1,9 +1,13 @@
 package com.dev.kimura.CadastroAgentes.Agentes;
 
 import org.aspectj.weaver.loadtime.Agent;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.annotation.Repeatable;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping()
@@ -11,47 +15,80 @@ public class AgenteController {
 
     private AgenteService agenteService;
 
-    AgenteController(AgenteService agenteService){
+    AgenteController(AgenteService agenteService) {
         this.agenteService = agenteService;
     }
 
     @GetMapping("/")
-    public String bemVindo(){
+    public String bemVindo() {
         return "Bem vindo! este projeto é obra de Rafael Kimura, e provavelmente ele ainda estava treinando Spring!";
     }
 
 
-
     // Exibir todos os Agentes (Read)
     @GetMapping("/listarAgente")
-    public List<AgenteDTO> listarAgentes(){
-        return agenteService.listarAgentes();
+    public ResponseEntity<List<AgenteDTO>> listarAgentes() {
+      List<AgenteDTO> listaAgentes = agenteService.listarAgentes();
+
+        return ResponseEntity.ok(listaAgentes);
+
     }
 
     // Exibir apenas um Agente (Read)
     @GetMapping("/listarAgente/{id}")
-    public AgenteDTO listarAgentePorID(@PathVariable Long id){
-        return agenteService.listarAgentePorId(id);
+    public ResponseEntity<String> listarAgentePorID(@PathVariable Long id) {
+
+        if(agenteService.listarAgentePorId(id) != null) {
+            AgenteDTO agenteEncontrado = agenteService.listarAgentePorId(id);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body("Agentes encontrados:\n" + agenteEncontrado);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Agente Não encontrado na lista");
     }
 
     // Adicionar um Agente (Create)
     @PostMapping("/criarAgente")
-    public AgenteDTO criarAgente(@RequestBody AgenteDTO agente){
-        return agenteService.criarNovoAgente(agente);
+    public ResponseEntity<String> criarAgente(@RequestBody AgenteDTO agente) {
+
+        AgenteDTO novoAgente = agenteService.criarNovoAgente(agente);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Agente criado com sucesso: " + "(ID):" + novoAgente.getId() + " " + novoAgente.getNome());
     }
 
     // Alterar os dados dos Agentes (Update)
-    @PutMapping ("/atualizarAgente/{id}")
-    public AgenteDTO atualizarAgentePorID(@PathVariable Long id, @RequestBody AgenteDTO agenteAtualizado){
-       return agenteService.atualizarAgente(id, agenteAtualizado);
+    @PutMapping("/atualizarAgente/{id}")
+    public ResponseEntity<String> atualizarAgentePorID(@PathVariable Long id, @RequestBody AgenteDTO agenteAtualizado) {
+
+        if (agenteService.listarAgentePorId(id) != null) {
+
+            AgenteDTO agenteAntigo = agenteService.listarAgentePorId(id);
+            AgenteDTO agenteAlterado = agenteService.atualizarAgente(id, agenteAtualizado);
+
+            Map<String, Object> resposta = Map.of(
+                    "nome", agenteAntigo.getNome() + " -> " + agenteAtualizado.getNome(),
+                    "email", agenteAntigo.getEmail() + " -> " + agenteAtualizado.getEmail(),
+                    "idade", agenteAntigo.getIdade() + " -> " + agenteAtualizado.getIdade(),
+                    "nivelDeDemanda", agenteAntigo.getNivelDeDemanda() + " -> " + agenteAtualizado.getNivelDeDemanda()
+            );
+            return ResponseEntity.status(HttpStatus.OK).body("Agente Alterado com Sucesso!\n" + resposta);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Agente Não encontrado para edição");
+
     }
 
     // Deletar um Agente (Delete)
-    @DeleteMapping ("/deletarAgente/{id}")
-    public void deletarAgentePorID(@PathVariable Long id){
-        agenteService.deletarAgentePorId(id);
-    }
+    @DeleteMapping("/deletarAgente/{id}")
+    public ResponseEntity<String> deletarAgentePorID(@PathVariable Long id) {
 
+        if (agenteService.listarAgentePorId(id) != null) {
+            agenteService.deletarAgentePorId(id);
+            return ResponseEntity.status(HttpStatus.OK).body("Agente "+ id + "deletada com Sucesso!");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Agente não existe");
+
+
+    }
 
 
 }
--- src/main/java/com/dev/kimura/CadastroAgentes/Tarefas/TarefasController.java
+++ src/main/java/com/dev/kimura/CadastroAgentes/Tarefas/TarefasController.java
@@ -1,8 +1,12 @@
 package com.dev.kimura.CadastroAgentes.Tarefas;
 
+import com.dev.kimura.CadastroAgentes.Agentes.AgenteDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("tarefas")
@@ -15,34 +19,65 @@ public class TarefasController {
     }
 
     @GetMapping("/listarTarefas")
-    public List<TarefasDTO> listarTarefas(){
+    public ResponseEntity<List<TarefasDTO>> listarTarefas(){
 
-        return tarefasService.listarTarefas();
+        List<TarefasDTO> tarefasEncontradas = tarefasService.listarTarefas();
+
+        return ResponseEntity.ok(tarefasEncontradas);
     }
 
     @GetMapping("/listarTarefa/{id}")
-    public TarefasDTO listarTarefasPorId(@PathVariable Long id){
+    public ResponseEntity<String> listarTarefasPorId(@PathVariable Long id){
 
-        return tarefasService.listarTarefaPorId(id);
+        if(tarefasService.listarTarefaPorId(id) != null){
+        TarefasDTO tarefaEncontrada = tarefasService.listarTarefaPorId(id);
+        return ResponseEntity.ok("Tarefa Retornarda: "+ tarefaEncontrada.getDescricao());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Tarefa não encontrada na agenda dos Agentes");
+
     }
 
 
     @PostMapping("/criarTarefa")
-    public TarefasDTO criarTarefa(@RequestBody TarefasDTO tarefasDTO){
+    public ResponseEntity<String> criarTarefa(@RequestBody TarefasDTO tarefasDTO){
 
-        return tarefasService.criarTarefa(tarefasDTO);
+        TarefasDTO novaTarefa = tarefasService.criarTarefa(tarefasDTO);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Tarefa criada com sucesso: " + "(ID):" + novaTarefa.getId() + " " + novaTarefa.getDescricao());
+
     }
 
     @PutMapping("/atualizaTarefa/{id}")
-    public TarefasDTO atualizaTarefa(@PathVariable Long id, @RequestBody TarefasDTO tarefaAtualizada){
+    public ResponseEntity<String> atualizaTarefa(@PathVariable Long id, @RequestBody TarefasDTO tarefaAtualizada){
 
-        return tarefasService.atualizaTarefas(id, tarefaAtualizada);
+        if (tarefasService.listarTarefaPorId(id) != null) {
+
+            TarefasDTO tarefaAnterior = tarefasService.listarTarefaPorId(id);
+            TarefasDTO tarefaAlterada = tarefasService.atualizaTarefas(id, tarefaAtualizada);
+
+            Map<String, Object> resposta = Map.of(
+                    "Descrição", tarefaAnterior.getDescricao() + " -> " + tarefaAlterada.getDescricao(),
+                    "Agentes responsáveis:", "Anteriores: " + tarefaAnterior.getAgentes() + "\n Novos:" + tarefaAlterada.getAgentes()
+            );
+            return ResponseEntity.status(HttpStatus.OK).body("Tarefa alterada com Sucesso!\n" + resposta);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Tarefa Não encontrado para edição");
+
+
     }
 
     @DeleteMapping("/deletarTarefa/{id}")
-    public void deletarTarefaPorId(@PathVariable Long id){
+    public ResponseEntity<String> deletarTarefaPorId(@PathVariable Long id){
 
-        tarefasService.deletarTarefaPorId(id);
+
+
+        if (tarefasService.listarTarefaPorId(id) != null) {
+            tarefasService.deletarTarefaPorId(id);
+            return ResponseEntity.status(HttpStatus.OK).body("Tarefa " + id + "Deletada com Sucesso!");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Tarefa não existe");
+
+
     }
 
 }
