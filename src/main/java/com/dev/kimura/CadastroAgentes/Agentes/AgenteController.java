120b21996c467bb66943b768ea044b0a976225c1
Kimura <rafaelkimura57@gmail.com>
2025-10-27 00:02:28 -0300

    docs: adicionar documentação dos endpoints de Agentes e Tarefas com Swagger e springdoc-openapi

--- src/main/java/com/dev/kimura/CadastroAgentes/Agentes/AgenteController.java
+++ src/main/java/com/dev/kimura/CadastroAgentes/Agentes/AgenteController.java
@@ -1,5 +1,9 @@
 package com.dev.kimura.CadastroAgentes.Agentes;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.aspectj.weaver.loadtime.Agent;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +24,7 @@ public class AgenteController {
     }
 
     @GetMapping("/")
+    @Operation(summary = "Boas vindas ao Projeto", description = "Essa rota é a padrão inicial do meu projeto")
     public String bemVindo() {
         return "Bem vindo! este projeto é obra de Rafael Kimura, e provavelmente ele ainda estava treinando Spring!";
     }
@@ -27,6 +32,7 @@ public class AgenteController {
 
     // Exibir todos os Agentes (Read)
     @GetMapping("/listarAgente")
+    @Operation(summary = "Listagem de todos os Agentes", description = "Essa rota é responsável por retornar uma lista de todos os agentes cadastrados")
     public ResponseEntity<List<AgenteDTO>> listarAgentes() {
       List<AgenteDTO> listaAgentes = agenteService.listarAgentes();
 
@@ -36,7 +42,13 @@ public class AgenteController {
 
     // Exibir apenas um Agente (Read)
     @GetMapping("/listarAgente/{id}")
-    public ResponseEntity<String> listarAgentePorID(@PathVariable Long id) {
+    @Operation(summary = "Listagem de um único Agente por ID", description = "Essa rota é responsável por retornar um Agente cadastrado de acordo com o ID inserido")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Agente encontrado: Agente"),
+            @ApiResponse(responseCode = "400", description = "Agente Não encontrado na lista")
+    })
+    public ResponseEntity<String> listarAgentePorID(@Parameter(description = "Usuário manda o ID no caminho da requisição")
+                                                    @PathVariable Long id) {
 
         if(agenteService.listarAgentePorId(id) != null) {
             AgenteDTO agenteEncontrado = agenteService.listarAgentePorId(id);
@@ -48,17 +60,30 @@ public class AgenteController {
 
     // Adicionar um Agente (Create)
     @PostMapping("/criarAgente")
+    @Operation(summary = "Cria um novo Agente", description = "Essa rota é responsável por permitir a criação de um novo agente com base nos dados básicos fornecidos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Agente criado com sucesso: (ID): id - nome do agente"),
+            @ApiResponse(responseCode = "400", description = "Erro na criação do Agente")
+    })
     public ResponseEntity<String> criarAgente(@RequestBody AgenteDTO agente) {
 
         AgenteDTO novoAgente = agenteService.criarNovoAgente(agente);
 
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body("Agente criado com sucesso: " + "(ID):" + novoAgente.getId() + " " + novoAgente.getNome());
+                .body("Agente criado com sucesso: " + "(ID):" + novoAgente.getId() + " - " + novoAgente.getNome());
     }
 
     // Alterar os dados dos Agentes (Update)
     @PutMapping("/atualizarAgente/{id}")
-    public ResponseEntity<String> atualizarAgentePorID(@PathVariable Long id, @RequestBody AgenteDTO agenteAtualizado) {
+    @Operation(summary = "Altera agente por ID", description = "Essa rota é responsável por alterar os dados de um Agente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Agente Alterado com Sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Agente Não encontrado para edição")
+    })
+    public ResponseEntity<String> atualizarAgentePorID(@Parameter(description = "Usuário manda o ID no caminho da requisição")
+                                                       @PathVariable Long id,
+                                                       @Parameter(description = "Usuário os dados atualizados no corpo da requisição")
+                                                       @RequestBody AgenteDTO agenteAtualizado) {
 
         if (agenteService.listarAgentePorId(id) != null) {
 
@@ -79,7 +104,13 @@ public class AgenteController {
 
     // Deletar um Agente (Delete)
     @DeleteMapping("/deletarAgente/{id}")
-    public ResponseEntity<String> deletarAgentePorID(@PathVariable Long id) {
+    @Operation(summary = "Deleta o agente por ID", description = "Essa rota é responsável por deletar os dados de um Agente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Agente id do agente  deletada com Sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Agente não existe")
+    })
+    public ResponseEntity<String> deletarAgentePorID( @Parameter(description = "Usuário manda o ID no caminho da requisição")
+                                                      @PathVariable Long id) {
 
         if (agenteService.listarAgentePorId(id) != null) {
             agenteService.deletarAgentePorId(id);
--- src/main/java/com/dev/kimura/CadastroAgentes/Tarefas/TarefasController.java
+++ src/main/java/com/dev/kimura/CadastroAgentes/Tarefas/TarefasController.java
@@ -1,6 +1,10 @@
 package com.dev.kimura.CadastroAgentes.Tarefas;
 
 import com.dev.kimura.CadastroAgentes.Agentes.AgenteDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +23,7 @@ public class TarefasController {
     }
 
     @GetMapping("/listarTarefas")
+    @Operation(summary = "Listagem de todos os Tarefas", description = "Essa rota é responsável por retornar uma lista de todos as tarefas cadastradas")
     public ResponseEntity<List<TarefasDTO>> listarTarefas(){
 
         List<TarefasDTO> tarefasEncontradas = tarefasService.listarTarefas();
@@ -27,7 +32,13 @@ public class TarefasController {
     }
 
     @GetMapping("/listarTarefa/{id}")
-    public ResponseEntity<String> listarTarefasPorId(@PathVariable Long id){
+    @Operation(summary = "Listagem de uma única Tarefa por ID", description = "Essa rota é responsável por retornar uma Tarefa cadastrada de acordo com o ID inserido")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tarefa Retornarda: descrição da tarefa"),
+            @ApiResponse(responseCode = "400", description = "Tarefa não encontrada na agenda dos Agentes")
+    })
+    public ResponseEntity<String> listarTarefasPorId(@Parameter(description = "Usuário manda o ID no caminho da requisição")
+                                                     @PathVariable Long id){
 
         if(tarefasService.listarTarefaPorId(id) != null){
         TarefasDTO tarefaEncontrada = tarefasService.listarTarefaPorId(id);
@@ -39,18 +50,33 @@ public class TarefasController {
 
 
     @PostMapping("/criarTarefa")
+    @Operation(summary = "Cria uma nova Tarefa", description = "Essa rota é responsável por permitir a criação de um nova tarefa com base nos dados básicos fornecidos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Tarefa criada com sucesso: (ID): id - descrição da tarefa"),
+            @ApiResponse(responseCode = "400", description = "Erro na criação da Tarefa")
+    })
     public ResponseEntity<String> criarTarefa(
+            @Parameter(description = "Converte o JSON recebido no DTO de tarefas")
             @RequestBody TarefasDTO tarefasDTO,
+            @Parameter(description = "Lista de IDs dos agentes associados à tarefa (opcional)")
             @RequestParam(required = false) List<Long> agenteIds) {
 
         TarefasDTO novaTarefa = tarefasService.criarTarefa(tarefasDTO, agenteIds);
 
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body("Tarefa criada com sucesso: (ID): " + novaTarefa.getId() + " " + novaTarefa.getDescricao());
+                .body("Tarefa criada com sucesso: (ID): " + novaTarefa.getId() + " - " + novaTarefa.getDescricao());
     }
 
     @PutMapping("/atualizaTarefa/{id}")
-    public ResponseEntity<String> atualizaTarefa(@PathVariable Long id, @RequestBody TarefasDTO tarefaAtualizada){
+    @Operation(summary = "Altera Tarefa por ID", description = "Essa rota é responsável por alterar os dados de uma Tarefa")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tarefa alterada com Sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Tarefa Não encontrado para edição")
+    })
+    public ResponseEntity<String> atualizaTarefa(@Parameter(description = "Usuário manda o ID no caminho da requisição")
+                                                 @PathVariable Long id,
+                                                 @Parameter(description = "Converte o JSON recebido no DTO de tarefas para atualização de dados")
+                                                 @RequestBody TarefasDTO tarefaAtualizada){
 
         if (tarefasService.listarTarefaPorId(id) != null) {
 
@@ -69,13 +95,17 @@ public class TarefasController {
     }
 
     @DeleteMapping("/deletarTarefa/{id}")
-    public ResponseEntity<String> deletarTarefaPorId(@PathVariable Long id){
-
-
+    @Operation(summary = "Deleta o tarefa por ID", description = "Essa rota é responsável por deletar os dados de uma Tarefa")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tarefa id da tarefa deletada com Sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Tarefa não existe")
+    })
+    public ResponseEntity<String> deletarTarefaPorId(@Parameter(description = "Usuário manda o ID no caminho da requisição")
+                                                     @PathVariable Long id){
 
         if (tarefasService.listarTarefaPorId(id) != null) {
             tarefasService.deletarTarefaPorId(id);
-            return ResponseEntity.status(HttpStatus.OK).body("Tarefa " + id + "Deletada com Sucesso!");
+            return ResponseEntity.status(HttpStatus.OK).body("Tarefa " + id + " Deletada com Sucesso!");
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Tarefa não existe");
 
